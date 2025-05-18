74185f1dca4991f724c2415650ee8f43c3d6f03f
jinno <klvx01@gmail.com>
2025-05-18 19:27:41 +0900

    feat: refactor imports in DefaultInputs and main files; add tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    './free-react-tailwind-admin-dashboard-main/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
